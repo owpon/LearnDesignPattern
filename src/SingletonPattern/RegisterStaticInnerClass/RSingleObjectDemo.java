03a6fd507384b18d44020a6c3ebbe3d09b04f1ea
里夫 <zivchan.tw@gmail.com>
2021-05-05 20:25:35 +0800

    增加main方法

---
+++ src/SingletonPattern/RegisterStaticInnerClass/RSingleObjectDemo.java
@@ -0,0 +1,12 @@
+package SingletonPattern.RegisterStaticInnerClass;
+
+/**
+ * @author ziv
+ * @date 2021-05-05 8:20 下午
+ * @description
+ */
+public class RSingleObjectDemo {
+    public static void main(String[] args) {
+
+    }
+}
